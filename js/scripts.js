fa2bfa3b11e2213103d21003f862701f54483ffa
uuniko <zhushengjie789@163.com>
2017-08-20 09:46:18 +0800

    Site updated: 2017-08-20 09:46:16

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,190 @@
-// build time:Thu Aug 17 2017 10:49:37 GMT+0800 (中国标准时间)
-$(document).ready(function(e){"use strict";e(window).scroll(function(){if(e(document).scrollTop()>10){e(".lightnav .navbar-inner").addClass("lightnav-alt");e(".darknav .navbar-inner").addClass("darknav-alt")}else{e(".lightnav .navbar-inner").removeClass("lightnav-alt");e(".darknav .navbar-inner").removeClass("darknav-alt")}});e(document).ready(function(e){e.slidebars();e("[data-toggle=popover]").popover({html:true});e("html").niceScroll({smoothscroll:true,autohidemode:false,zindex:"100",scrollspeed:60,mousescrollstep:40,gesturezoom:false,horizrailenabled:false,cursorcolor:"#151515",boxzoom:false,cursorborder:"0 solid #202020",cursorborderradius:"5px",cursorwidth:9,enablemousewheel:true,background:"rgba(255,255,255,0.7)"});e(".animsition").animsition({inClass:"fade-in",outClass:"fade-out-down-sm",inDuration:900,outDuration:800,linkElement:".animsition-link",loading:true,loadingParentElement:"body",loadingClass:"animsition-loading",unSupportCss:["animation-duration","-webkit-animation-duration","-o-animation-duration"],overlay:false,overlayClass:"animsition-overlay-slide",overlayParentElement:"body"});var a=new WOW({animateClass:"animated",offset:250,mobile:false,live:true});(new WOW).init();if(!Modernizr.touch){jQuery(function(e){var a=e(".herofade");e(window).on("scroll",function(){var n=e(this).scrollTop();a.css({"margin-top":-(n/0)+"px",opacity:0})})});jQuery(function(e){var a=e(".videofade");e(window).on("scroll",function(){var n=e(this).scrollTop();a.css({"margin-top":-(n/0)+"px",opacity:.8-n/1600})})});jQuery(function(e){var a=e(".headerfade");e(window).on("scroll",function(){var n=e(this).scrollTop();a.css({"margin-top":-(n/0)+"px",opacity:.9-n/300})})})}e("div.navbar-fixed-top").autoHidingNavbar({animationDuration:400,hideOffset:0});e("#sidebar").affix({offset:{top:500}});var n=e(document.body);var o=e(".navbar").outerHeight(true)+80;n.scrollspy({target:"#leftcol",offset:o});e(".map-cover").click(function(){e(".map-cover").fadeOut("slow")});e(".collapse").on("show.bs.collapse",function(){var a=e(this).attr("id");e('a[href="#'+a+'"]').closest(".panel-heading").addClass("active-panel");e('a[href="#'+a+'"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-up"></i>')});e(".collapse").on("hide.bs.collapse",function(){var a=e(this).attr("id");e('a[href="#'+a+'"]').closest(".panel-heading").removeClass("active-panel");e('a[href="#'+a+'"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-down"></i>')});if(navigator.userAgent.match(/IEMobile\/10\.0/)){var i=document.createElement("style");i.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"));document.querySelector("head").appendChild(i)}})}(jQuery));$(document).ready(function(e){e(".sb-toggle-submenu").off("click").on("click",function(){$submenu=e(this).parent().children(".sb-submenu");e(this).add($submenu).toggleClass("sb-submenu-active");if($submenu.hasClass("sb-submenu-active")){$submenu.slideDown(200)}else{$submenu.slideUp(200)}})});
-//rebuild by neat 
+$(document).ready(function($) {
+    "use strict";
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 10) {
+            $(".lightnav .navbar-inner").addClass("lightnav-alt");
+            $(".darknav .navbar-inner").addClass("darknav-alt");
+        } else {
+            $(".lightnav .navbar-inner").removeClass("lightnav-alt");
+            $(".darknav .navbar-inner").removeClass("darknav-alt");
+        }
+    });
+
+	$(document).ready(function($) {
+
+		// Slidebars off-canvas menu
+		$.slidebars();
+
+		// Popovers [Hover]
+		$("[data-toggle=popover]")
+			.popover({
+				html:true
+			}
+		);
+
+		$("html").niceScroll({
+			smoothscroll: true, // scroll with ease movement
+			autohidemode: false,
+			zindex: "100", // change z-index for scrollbar div
+        	scrollspeed: 60, // scrolling speed
+        	mousescrollstep: 40,
+        	gesturezoom: false,
+        	horizrailenabled: false,
+        	cursorcolor: "#151515",
+	        boxzoom: false,
+	        cursorborder: "0 solid #202020",
+	        cursorborderradius: "5px",
+	        cursorwidth: 9,
+	        enablemousewheel: true,
+	        background: "rgba(255,255,255,0.7)",
+		});
+
+		// Page transitions
+		$(".animsition").animsition({
+
+			inClass               :   'fade-in',
+			outClass              :   'fade-out-down-sm',
+			inDuration            :    900,
+			outDuration           :    800,
+			linkElement           :   '.animsition-link',
+			//e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+			loading               :    true,
+			loadingParentElement  :   'body', //animsition wrapper element
+			loadingClass          :   'animsition-loading',
+			unSupportCss          : [ 'animation-duration',
+			                          '-webkit-animation-duration',
+			                          '-o-animation-duration'
+			                        ],
+			//"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+			//The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+
+			overlay               :   false,
+
+			overlayClass          :   'animsition-overlay-slide',
+			overlayParentElement  :   'body'
+		});
+
+		// WOW plugin settings
+        var wow = new WOW(
+          { animateClass: 'animated', // set our global css classT (default is animated)
+            offset: 250, // set distance to content until it triggers (default is 0)
+            mobile: false, // remove animations for mobiles/tablets (default is true)
+            live: true }); // act on asynchronously loaded content (default is true)
+        new WOW().init();
+
+		// Functionailty constraints for mobile
+		if (!Modernizr.touch) {
+		    jQuery(function ($) {
+		        // Hero & page-header fade-in effect
+		        var divs = $('.herofade');
+		        $(window).on('scroll', function () {
+		            var st = $(this).scrollTop();
+		            divs.css({
+		                'margin-top': -(st / 0) + "px",
+                    'opacity': 0
+		                // 'opacity': 0.9 - st / 1600
+		            });
+		        });
+		    });
+
+		    jQuery(function ($) {
+		        // Hero & page-header fade-in effect
+		        var divs = $('.videofade');
+		        $(window).on('scroll', function () {
+		            var st = $(this).scrollTop();
+		            divs.css({
+		                'margin-top': -(st / 0) + "px",
+		                'opacity': 0.8 - st / 1600
+		            });
+		        });
+		    });
+
+		    jQuery(function ($) {
+		        // Hero & page-header fade-in effect
+		        var divs = $('.headerfade');
+		        $(window).on('scroll', function () {
+		            var st = $(this).scrollTop();
+		            divs.css({
+		                'margin-top': -(st / 0) + "px",
+		                'opacity': 0.9 - st / 300
+		            });
+		        });
+		    });
+		}
+
+		// autohide navbar on scroll
+		$("div.navbar-fixed-top").autoHidingNavbar({
+			animationDuration: 400,
+			hideOffset: 0,
+		});
+
+		// faq's floating sidebar (left)
+	    $('#sidebar').affix({
+	    	offset: {
+	    		top: 500
+	    	}
+		});
+
+	    // Scrollspy for scrollto links in floating faq sidebar
+		var $body   = $(document.body);
+		var navHeight = $('.navbar').outerHeight(true) + 80;
+
+		$body.scrollspy({
+			target: '#leftcol',
+			offset: navHeight
+		});
+
+		// fade out map cover (contact.html)
+	    $(".map-cover").click(function () {
+	        $(".map-cover").fadeOut("slow");
+	    });
+
+		// Collapsible panels for faq's and careers
+	    $('.collapse').on('show.bs.collapse', function() {
+	        var id = $(this).attr('id');
+	        $('a[href="#' + id + '"]').closest('.panel-heading').addClass('active-panel');
+	        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-up"></i>');
+	    });
+	    $('.collapse').on('hide.bs.collapse', function() {
+	        var id = $(this).attr('id');
+	        $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('active-panel');
+	        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-down"></i>');
+	    });
+
+	    /*!
+	     * IE10 viewport hack for Surface/desktop Windows 8 bug
+	     * Copyright 2014 Twitter, Inc.
+	     * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+	     * details, see http://creativecommons.org/licenses/by/3.0/.
+	     */
+	    // See the Getting Started docs for more information:
+	    // http://getbootstrap.com/getting-started/#support-ie10-width
+	    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+	        var msViewportStyle = document.createElement('style');
+	        msViewportStyle.appendChild(
+	            document.createTextNode(
+	                '@-ms-viewport{width:auto!important}'
+	            )
+	        );
+	        document.querySelector('head').appendChild(msViewportStyle);
+	    }
+
+	}); // Document Ready
+
+}(jQuery)); // End "use strict"
+
+// Enable dropdown sub-menus in off-canvas navigation
+$(document).ready(function($) {
+	$('.sb-toggle-submenu').off('click') // Stop submenu toggle from closing Slidebars.
+		.on('click', function() {
+			$submenu = $(this).parent().children('.sb-submenu');
+			$(this).add($submenu).toggleClass('sb-submenu-active'); // Toggle active class.
+
+			if ($submenu.hasClass('sb-submenu-active')) {
+			$submenu.slideDown(200);
+			} else {
+			$submenu.slideUp(200);
+		}
+	});
+});
